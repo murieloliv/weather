3a22d2a1a25c24d776bf4230d170b89d81599f47
murieloliv <murieloliv@icloud.com>
2025-02-05 20:54:53 -0300

    Atualização Model

--- lib/modules/weather/weather.dart
+++ lib/modules/weather/weather.dart
@@ -1,21 +1,33 @@
 class Weather {
   final String cidade;
-  final double temperatura;
+  final int temperatura;
   final String condicao;
-  final String condicaoTraduzida;
+  final double tempMin;
+  final double tempMax;
 
   Weather({
     required this.cidade,
     required this.temperatura,
     required this.condicao,
-    required this.condicaoTraduzida,
+    required this.tempMin,
+    required this.tempMax,
   });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
+    var results = json['results'];
+    var forecastList = results['forecast'];
+
+    double tempMin =
+        forecastList.isNotEmpty ? forecastList[0]['min'].toDouble() : 0.0;
+    double tempMax =
+        forecastList.isNotEmpty ? forecastList[0]['max'].toDouble() : 0.0;
     return Weather(
-        cidade: json['name'],
-        temperatura: json['main']['temp'].toDouble(),
-        condicao: json['weather'][0]['main'],
-        condicaoTraduzida: json['weather'][0]['description']);
+      cidade: results['city'] ?? 'Cidade Indisponível',
+      temperatura: results['temp'] ?? 0.0,
+      condicao: results['description'] ?? 'Condição Indisponível',
+      tempMin: tempMin,
+      tempMax: tempMax,
+      // tempMax: json['main']['temp_max'].toDouble(),
+    );
   }
 }
